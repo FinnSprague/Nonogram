e7fc5959cf1cddc89d818be0a1167b87e146de11
ayoeyesan <ayoeyesan@icloud.com>
2024-11-18 17:45:32 -0500

    Got rid of uneccessary wrapper function, and implemented new method for checking column correctness

--- Project/validity.ml
+++ Project/validity.ml
@@ -12,9 +12,9 @@
   - square list - column at column_index of grid
 *)
 
-let rec extract_column grid new_row comlumn_index = 
-  let base_column = List.map(fun row -> List.nth row comlumn_index) grid in
-  base_column @ [List.nth new_row comlumn_index]
+let rec extract_column grid new_row column_index = 
+  let base_column = List.map(fun row -> List.nth row column_index) grid in
+  base_column @ [List.nth new_row column_index]
 ;;
 
 (* 
@@ -28,8 +28,17 @@ let rec extract_column grid new_row comlumn_index =
   - boolean - true if proposed column conforms to constraints, false otherwise
 *)
 
-let column_constraint_check partial_column constraint = 
-  row_validity constraint partial_column
+let column_validator partial_column constraint = 
+  let rec column_validator_helper constraints current_column current_block = match constraints, current_column with
+  |[], [] -> current_block = 0
+  |[], EMP :: tl -> column_validator_helper [] tl current_block
+  |[], BOX :: _ -> false
+  |h :: tlc, BOX :: tl -> column_validator_helper (if current_block + 1 = h then tlc else  constraints) tl (if current_block + 1 = h then 0 else  current_block + 1)
+  |h :: _, EMP:: tl -> if current_block > 0 then false else column_validator_helper constraints tl 0
+  |h :: _, [] when current_block > 0 -> current_block = h
+  |_, [] -> true
+in
+column_validator_helper partial_column constraint 0
 
 ;;
 
@@ -39,23 +48,20 @@ let column_constraint_check partial_column constraint =
   PARAMS:
   - (grid : square list list) - nongram grid built up so far
   - (new_row : square list) - new permutation being added to grid
-  - (row_index : int) - index of row in grid
   - (column_constraints : int list list) - list of constraints placed on columns of grid
   
   RETURN:
   - boolean - true if all proposed columns when new row has been added conform to constraints, false otherwise
 *)
 
-let row_validity grid new_row row_index column_constrains = 
+let row_validity grid new_row column_constrains = 
   let rec validate_columns index = 
     if index >= List.length column_constrains then true
     else
-      let partial_column = extract_column grid new_row index row_index 
-    in
-      let constraint = List.nth column_constrains index 
-  in
-  if column_constraint_check partial_column constraint then validate_columns (index + 1)
-  else false
+      let partial_column = extract_column grid new_row index in
+      let constraint = List.nth column_constrains index in
+      if column_validator partial_column constraint then validate_columns (index + 1)
+      else false
 in
 validate_columns 0
 
