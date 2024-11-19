0836cbfcb08892b039e692a2040ea04ee16665d0
Finn Sprague <spraguef@tcd.ie>
2024-11-19 12:54:35 -0500

    up to date with Ayo's code

--- main.ml
+++ main.ml
@@ -3,7 +3,7 @@ exception Error of string
 type square = BOX | EMP | BLANK ;;
 
 type grid = square list list;;
-(* Printing methods *) 
+(* ---------- Printing methods ----------  *) 
 
 let format_square (sq : square ) : string = 
   if sq = BOX then "BOX" else if sq = EMP then "EMP" else "BLANK" 
@@ -26,7 +26,14 @@ let rec print_arr_square (lst : square list) : unit =
 let print_grid (g: grid) : unit  = 
     List.iter ( fun x -> print_arr_square x ) g ;; 
 
-(* Methods *)
+
+
+
+
+
+
+
+(* ---------- Finn's Permutation Generator ---------- *)
 let rec sum (lst : int list) : int = 
   match lst with
   | [] -> 0 
@@ -139,7 +146,86 @@ let compute_permutations (constraints : int list) (xLen : int) : square list lis
   complete_all_permutations permutations xLen 
 ;;
 
-(* Taisuke's code from here. *)
+
+
+
+
+
+
+
+
+(* ---------- Ayo's Validity Check ----------*)
+(* 
+  extract_column
+  - extracts a column from the grid, including the new row permutation being added 
+  PARAMS:
+  - (grid : square list list) - nongram grid built up so far
+  - (new_row : square list) - new permutation being added to grid
+  - (column_index : int) - index of column in grid
+  
+  
+  RETURN:
+  - square list - column at column_index of grid
+*)
+
+let rec extract_column grid new_row column_index = 
+  let base_column = List.map(fun row -> List.nth row column_index) grid in
+  base_column @ [List.nth new_row column_index]
+;;
+
+(* 
+  column_constraint_check
+  - checks extracted column against column constraints 
+  PARAMS:
+  - (partial_column : square list) - column of grid (which is not completed)
+  - (constraint : int list) - constraints placed on column of grid
+  
+  RETURN:
+  - boolean - true if proposed column conforms to constraints, false otherwise
+*)
+
+let column_validator (partial_column : square list)  ( cons : int list ) : bool = 
+  let rec column_validator_helper constraints current_column current_block = match constraints, current_column with
+  |[], [] -> current_block = 0
+  |[], EMP :: tl -> column_validator_helper [] tl current_block
+  |[], BOX :: _ -> false
+  |h :: tlc, BOX :: tl -> if current_block + 1 = h then column_validator_helper tlc tl 0
+  else column_validator_helper constraints tl (current_block + 1)
+  |h :: _, EMP:: tl -> if current_block > 0 then false else column_validator_helper constraints tl 0
+  |h :: _, [] when current_block > 0 -> current_block = h
+  |_, [] -> true
+  |_ -> false 
+in
+column_validator_helper cons partial_column 0
+
+;;
+
+(* 
+  row_validity
+  - checks if a given new row permutation will fit with previous rows and column constraints of all columns in grid
+  PARAMS:
+  - (grid : square list list) - nongram grid built up so far
+  - (new_row : square list) - new permutation being added to grid
+  - (column_constraints : int list list) - list of constraints placed on columns of grid
+  
+  RETURN:
+  - boolean - true if all proposed columns when new row has been added conform to constraints, false otherwise
+*)
+
+let row_validity (grid : square list list) (new_row : square list) 
+                 (column_constrains : int list list) : bool = 
+  let rec validate_columns index = 
+    if index >= List.length column_constrains then true
+    else
+      let partial_column = extract_column grid new_row index in
+      let cons = List.nth column_constrains index in
+      if column_validator partial_column cons then validate_columns (index + 1)
+      else false
+in
+validate_columns 0
+;;
+
+(* ---------- Taisuke's DFS ---------- . *)
 
 
 let convert_list_into_constraints (sl: square list): int list = 
